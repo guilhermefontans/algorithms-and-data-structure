43c03f06a54f338772b0e091e2d1296859c22a0c
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-06-28 23:48:07 -0300

    Create test case and class to implements binary search

---
+++ src/Search/BinarySearch.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Algorithms\Search;
+
+/**
+ * Class BinarySearch
+ *
+ * @package Algorithms\Search
+ */
+class BinarySearch
+{
+    /**
+     * @param array $array
+     * @param int $x
+     * @return float|int
+     */
+    public function search(array $array, int $x)
+    {
+        $left = 0;
+        $right = count($array) -1;
+
+        while ($left <= $right) {
+            $mid = $left + (ceil(($right - $left) / 2));
+
+            if ($array[$mid] == $x) {
+                return $mid;
+            }
+
+            if ($x < $array[$mid]) {
+                $right = $mid -1;
+            } else {
+                $left = $mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ tests/Search/BinarySearchTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Search;
+
+use Algorithms\Search\BinarySearch;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class BinarySearchTest
+ * @package Tests\Search
+ */
+class BinarySearchTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testSearch() {
+        $array = [3, 6, 8, 13, 35, 56, 78, 89, 92, 95];
+
+        $binarySearch = new BinarySearch();
+
+        $key = $binarySearch->search($array, 78);
+        $this->assertEquals(6, $key);
+
+        $key = $binarySearch->search($array, 5);
+        $this->assertEquals(-1, $key);
+    }
+}
