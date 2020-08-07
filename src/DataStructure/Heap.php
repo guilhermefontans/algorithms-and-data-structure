7ece67f1540c81aef6fdb2aff064c2be7cd2258b
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-08-06 22:38:19 -0300

    Added more methods to heap

--- src/DataStructure/Heap.php
+++ src/DataStructure/Heap.php
@@ -12,11 +12,71 @@ namespace Algorithms\DataStructure;
  */
 class Heap
 {
+
+    protected $size = 0;
+
+    protected $items = [];
+
+    public function peek()
+    {
+        return $this->items[0];
+    }
+
+    public function pool()
+    {
+        $this->items[0] = $this->items[$this->size -1];
+        $this->size--;
+        $this->heapifyDown();
+    }
+
+    public function add(int $item)
+    {
+        $this->items[$this->size] = $item;
+        $this->size++;
+        $this->heapifyUp();
+    }
+
+    private function swap(int $firstIndex, int $secondIndex)
+    {
+        $tmp = $this->items[$firstIndex];
+        $this->items[$firstIndex] = $this->items[$secondIndex];
+        $this->items[$secondIndex] = $tmp;
+    }
+
+    public function heapifyUp()
+    {
+        $index = $this->size -1;
+
+        while ($this->hasParent($index) && $this->getParent($index) > $this->items[$index]) {
+            $this->swap($this->getParentIndex($index), $index);
+            $index = $this->getParentIndex($index);
+        }
+    }
+
+    public function heapifyDown()
+    {
+        $index = 0;
+        while ($this->hasLeftChild($index)) {
+            $smallerChildIndex = $this->getLeftChildIndex($index);
+            if ($this->hasRightChild($index) && $this->getRightChild($index) < $this->getLeftChild($index)) {
+                $smallerChildIndex = $this->getRightChildIndex($index);
+            }
+
+            if ($this->items[$index] < $this->items[$smallerChildIndex]) {
+                break;
+            } else {
+                $this->swap($index, $smallerChildIndex);
+            }
+
+            $index = $smallerChildIndex;
+        }
+    }
+
     /**
      * @param  int       $parentIndex
      * @return float|int
      */
-    public function getLeftChildIndex(int $parentIndex)
+    private function getLeftChildIndex(int $parentIndex)
     {
         return (2 * $parentIndex) + 1;
     }
@@ -25,17 +85,71 @@ class Heap
      * @param  int       $parentIndex
      * @return float|int
      */
-    public function getRightChildIndex(int $parentIndex)
+    private function getRightChildIndex(int $parentIndex)
     {
         return (2 * $parentIndex) + 2;
     }
 
     /**
      * @param  int   $childIndex
-     * @return float
+     * @return int
      */
-    public function getParentIndex(int $childIndex): float
+    private function getParentIndex(int $childIndex)
     {
         return floor(($childIndex - 1) / 2);
+    }
+
+    /**
+     * @param int $index
+     * @return mixed
+     */
+    private function getLeftChild(int $index)
+    {
+        return $this->items[$this->getLeftChild($index)];
+    }
+
+    /**
+     * @param int $index
+     * @return mixed
+     */
+    private function getRightChild(int $index)
+    {
+        return $this->items[$this->getRightChild($index)];
+    }
+
+    /**
+     * @param int $index
+     * @return mixed
+     */
+    private function getParent(int $index)
+    {
+        return $this->items[$this->getParent($index)];
+    }
+
+    /**
+     * @param int $index
+     * @return bool
+     */
+    private function hasParent(int $index)
+    {
+        return $this->getParentIndex($index) >= 0;
+    }
+
+    /**
+     * @param int $index
+     * @return bool
+     */
+    private function hasLeftChild(int $index)
+    {
+        return $this->getLeftChildIndex($index) < $this->size;
+    }
+
+    /**
+     * @param int $index
+     * @return bool
+     */
+    private function hasRightChild(int $index)
+    {
+        return $this->getRightChildIndex($index) < $this->size;
     }
 }
---
+++ tests/DataStructure/HeapTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Tests\DataStructure;
+
+use Algorithms\DataStructure\Heap;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class HeapTest
+ *
+ * @package Tests\DataStructure
+ */
+class HeapTest extends TestCase
+{
+
+    public function testHeap()
+    {
+        //$heap = new Heap();
+
+        //$heap->add(10);
+        //$heap->add(15);
+
+
+        //print_r($heap);
+    }
+}
