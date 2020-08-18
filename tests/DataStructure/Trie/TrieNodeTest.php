4a09d6e12759caaa663a9159c284481f1657b74f
Guilherme Fontans <guilherme.fontans@gmail.com>
2020-08-17 23:56:56 -0300

    Test to trie node

---
+++ tests/DataStructure/Trie/TrieNodeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\DataStructure\Trie;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class TrieNodeTest
+ * @package Tests\DataStructure\Trie
+ */
+class TrieNodeTest extends TestCase
+{
+
+}
